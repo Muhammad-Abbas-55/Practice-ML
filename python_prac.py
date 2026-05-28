0c2d72cd51f11ca6cb809324b8919e723fcd0415
Abbas <abbas85678@gmail.com>
2026-05-28 10:11:59 +0500

    Removed venv

---
+++ python_prac.py
@@ -0,0 +1,86 @@
+# num1 = float(input("Enter first no "))
+# num2 = float(input("Enter second no "))
+# sum = num1 + num2
+# print("Sum is", sum)
+
+print("Hello World")
+
+# a = float(input("Enter first no "))
+# area = a * a
+# print("Area of a Square is: ", area)
+
+# num1 = float(input("Enter first no "))
+# num2 = float(input("Enter second no "))
+# avg = (num1 + num2)/2
+# print("Avarage is : ", avg)
+
+# a = float(input("Enter first no "))
+# b = float(input("Enter second no "))
+# c = a >= b
+# print("First no is Greater : ", c)
+
+
+# a = float(input("Enter first no "))
+# b = a % 2
+# if( b == 0 ):
+#     print ("Given no is even")
+# else:
+#     print ("Given no is odd")
+
+
+# a = float(input("Enter first no "))
+# # b = a % 2
+# if( a % 2 == 0 ):
+#     print ("Given no is even")
+# else:
+#     print ("Given no is odd")
+
+
+# a = float(input("Enter First no "))
+# b = float(input("Enter Second no "))
+# c = float(input("Enter Third no "))
+# d = float(input("Enter Fourth no "))
+
+# if( a > b and a > c and a > d):
+#     print ("First no is greater", a)
+# elif(b > c and b > d):
+#     print ("Second no is greater", b)
+# elif(c > d):
+#     print("Third no is Greater", c)
+# else:
+#     print("Fourth no is Greater", d)
+
+
+
+# a = float(input("Enter First no "))
+# if( a % 7 == 0):
+#     print ("The no is multiple of 7 which is", a)
+# else:
+#     print("The no is Not multiple of 7 which is", a)
+
+
+
+# a = (input("Enter First movie name "))
+# b = (input("Enter Second no movie name "))
+# c = (input("Enter Third no movie name "))
+# movies = a,b,c
+# a=[]
+# a.append(movies)
+# print (type(a))
+# print ("Movies name is : ", a)
+
+
+# a = (input("Enter First movie name "))
+# b = (input("Enter Second no movie name "))
+# c = (input("Enter Third no movie name "))
+# movies=[]
+# movies.append(a)
+# movies.append(b)
+# movies.append(b)
+# print (type(movies))
+# print ("Movies name is : ", movies)
+
+
+
+
+
